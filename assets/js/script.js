1cd2c6bce8fdd6024f1732a417b686dfeb3b978b
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-15 19:07:38 +0800

    Add main function on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,8 @@ const menu = document.querySelector('#menu');
 const overlay = document.querySelector('#overlay');
 const btnCloseMenu = document.querySelectorAll('.nav_element');
 const btnOpenMenu = document.querySelector('#hamburger');
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+});
