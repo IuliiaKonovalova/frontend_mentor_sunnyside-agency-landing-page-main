11bb6f72b5cbf626e2bc70f7149baba1594ebe7d
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-15 19:13:44 +0800

    Add function to open menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,22 @@
 // Variables
 const menu = document.querySelector('#menu');
 const overlay = document.querySelector('#overlay');
-const btnCloseMenu = document.querySelectorAll('.nav_element');
+const btnCloseMenu = document.querySelectorAll('#nav_element_1');
 const btnOpenMenu = document.querySelector('#hamburger');
 
 document.addEventListener('DOMContentLoaded', function () {
 
+  //Open menu
+  btnOpenMenu.addEventListener('click', openMenu);
 
 });
+
+
+
+/**
+ * Open instructions modal window
+ */
+const openMenu = function () {
+  menu.classList.remove('nav_on_click');
+  overlay.classList.remove('overlay--hidden');
+};
