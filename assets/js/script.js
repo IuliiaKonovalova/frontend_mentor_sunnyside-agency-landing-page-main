dfd11a3e18a3d3a285e182eaf23ac2ce6c3f7bed
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-15 19:06:45 +0800

    Add main varialbes to js file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* jshint esversion:8 */
+
+// Variables
+const menu = document.querySelector('#menu');
+const overlay = document.querySelector('#overlay');
+const btnCloseMenu = document.querySelectorAll('.nav_element');
+const btnOpenMenu = document.querySelector('#hamburger');
